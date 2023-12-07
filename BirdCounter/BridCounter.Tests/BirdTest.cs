d327e08c7964934460cce5908f0344356dd6b38f
Jynxtorquilla1 <wojciech.szk@gmail.com>
2023-12-07 16:52:23 +0100

    Basic Test

--- BirdCounter/BridCounter.Tests/BirdTest.cs
+++ BirdCounter/BridCounter.Tests/BirdTest.cs
@@ -1,12 +1,28 @@
+using BirdCounter;
+
 namespace BridCounter.Tests
 {
     public class Tests
     {
-        
+
         [Test]
         public void Test1()
         {
-            var birdd = new BirdInFile("Go³¹b");
+            //arrange
+            var bird = new BirdInMemory("Go³¹b");
+            bird.AddNumber(1);
+            bird.AddNumber(30);
+            bird.AddNumber(300);
+
+            //act
+
+            var statistics = bird.GetStatistics();
+
+            //assert
+
+            Assert.AreEqual(331, statistics.Sum);
+
+
         }
     }
 }
