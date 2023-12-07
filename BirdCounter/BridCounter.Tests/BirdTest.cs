2f0322e8450bc8e0ab7ad7517663013a652b49ca
Jynxtorquilla1 <wojciech.szk@gmail.com>
2023-12-07 16:12:15 +0100

    Test project

---
+++ BirdCounter/BridCounter.Tests/BirdTest.cs
@@ -0,0 +1,12 @@
+namespace BridCounter.Tests
+{
+    public class Tests
+    {
+        
+        [Test]
+        public void Test1()
+        {
+            var birdd = new BirdInFile("Go³¹b");
+        }
+    }
+}
